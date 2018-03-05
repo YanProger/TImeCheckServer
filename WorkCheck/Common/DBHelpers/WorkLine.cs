f87e5d97ab4a8a3737394d5c4434404d070a1ad7
Yan <losevyan@gmail.com>
2018-03-06 00:06:09 +0500

    first

---
+++ WorkCheck/Common/DBHelpers/WorkLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.DBHelpers
+{
+    public class WorkLine
+    {
+        [Key]
+        public int WorkLineID { get; set; }
+        public string Name { get; set; }
+
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual ICollection<Period> Periods { get; set; }
+    }
+}

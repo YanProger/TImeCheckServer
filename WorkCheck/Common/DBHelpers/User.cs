0c08dd2f19390c96c8ba913a42a0adac36525bd5
Yan <losevyan@gmail.com>
2018-03-06 00:33:34 +0500

    hz

---
+++ WorkCheck/Common/DBHelpers/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.DBHelpers
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Mail { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<WorkLine> WorkLines { get; set; }
+    }
+}

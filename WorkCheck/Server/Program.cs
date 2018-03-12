f245b38f1f5713f33cb3f457c163b027ba327928
Yan <losevyan@gmail.com>
2018-03-12 05:08:34 +0500

    kestrel

--- WorkCheck/Server/Program.cs
+++ WorkCheck/Server/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
 
 namespace Server
 {
@@ -20,6 +22,16 @@ namespace Server
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                 .UseKestrel(options =>
+                 {
+                     options.Limits.MaxConcurrentConnections = 100;
+                     options.Limits.MaxRequestBodySize = 10 * 1024;
+                     options.Limits.MinRequestBodyDataRate =
+                         new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                     options.Limits.MinResponseDataRate =
+                         new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                     options.Listen(new IPAddress(new byte[] { 192,168,0,101}), 5000);
+                 })
                 .Build();
     }
 }
